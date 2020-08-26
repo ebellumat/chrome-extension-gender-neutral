1ff52521189617038c4529c7b3e9f38cf03ca2cd
Erasmo Bellumat <lbj.erasmo@gmail.com>
2020-08-25 21:12:01 -0300

    First commit

--- content_script.js
+++ content_script.js
@@ -1,35 +1,86 @@
-var playbackRate = 1;
+const pronouns = {
+  todes: ['todos', 'todas'],
+  elu: ['ele', 'ela'],
+  elus: ['eles', 'elas'],
+  delus: ['deles', 'delas'],
+  delu: ['dele', 'dela'],
+  aquelu: ['aquele', 'aquela'],
+  aquelus: ['aqueles', 'aquelas'],
+  amigues: ['amigos', 'amigas'],
+  amigue: ['amigo', 'amiga'],
+  nelu: ['nele', 'nela'],
+  nelus: ['neles', 'nelas'],
 
-const changeSpeed = (increase) => {
-  var videos =  document.querySelectorAll('video');
-  
-  for (video of videos) {
-    video.playbackRate = playbackRate;
-  };
+  //adjetivos
+  //a
+  amades: ['amados'],
+  atrasade: ['atrasado', 'atrasada'],
+  atrasades: ['atrasados', 'atrasadas'],
+  arrombade: ['arrombado', 'arrombada'],
+  arrombades: ['arrombados', 'arrombadas'],
+  //b
+  bonite: ['bonito', 'bonita'],
+  bonites: ['bonitos', 'bonitas'],
+  brasileire: ['brasileiro', 'brasileira'],
+  brasileires: ['brasileiros', 'brasileiras'],
+  //g
+  guerreire: ['guerreiro', 'guerreira'],
+  guerreires: ['guerreiros', 'guerreiras'],
+
+  cunhade: ['cunhado', 'cunhada'],
+  cunhades: ['cunhados', 'cunhadas'],
+
+  //l
+  linde: ['lindo', 'linda'],
+  lindes: ['lindos', 'lindas'],
+
 }
 
-document.addEventListener ('keypress', (event) => {
+var oldLocation = location.href;
 
-  if (event.shiftKey && event.key === '<') {
-    playbackRate -= 0.5;
-    changeSpeed();
-  }
+const changePronouns = () => {
+  if (location.href.includes('google.com')) return;
+  var innerHtml = document.body.innerHTML;
 
-  if (event.shiftKey && event.key === '>') {
-    playbackRate += 0.5;
-    changeSpeed();
+  Object.keys(pronouns).forEach(key => {
+    var newExp = new RegExp("\\b(?:" + pronouns[key].join("|") + ")\\b", "gi");
+
+    $('body :not(script)').contents().filter(function() {
+        return this.nodeType === 3;
+      }).replaceWith(function() {
+          return this.nodeValue.replace(newExp, key);
+      });
+    });
+}
+
+setInterval(function() {
+  if(location.href != oldLocation) {
+       // do your action
+       oldLocation = location.href
+       setTimeout(() => {
+         changePronouns();
+       });
   }
+}, 1000); // check every second
+
+window.addEventListener('popstate', function (event) {
+  setTimeout(() => {
+    changePronouns();
+  });
 });
 
-
-function myfunc(){
-  $(document).ready(function() { 
+document.onreadystatechange = () => {
+  if (document.readyState === 'complete') {
+    setTimeout(() => {
+      changePronouns();
+    });
 
     setTimeout(() => {
-      changeSpeed();
-    }, 1000);
-  });
+      changePronouns();
+    }, 1500);
 
-}
-
-$(document).ready(myfunc);
+    setTimeout(() => {
+      changePronouns();
+    }, 5000);
+  }
+};
